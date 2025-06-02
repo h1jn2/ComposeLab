c43b2a77a878877f219cbbddd63634dd956a2048
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-06-02 14:29:55 +0900

    Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,9 @@ android {
 
 dependencies {
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
+    implementation("io.coil-kt.coil3:coil-compose:3.1.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.1.0")
+    implementation("androidx.compose.material:material-icons-extended:1.7.6")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
